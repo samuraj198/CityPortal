894f2ca47314d0c08aa944e68b703cfd5658a16c
samuraj198 <daniil885@mail.ru>
2024-11-30 22:09:54 +0400

    change vite() on main.blade.php on asset

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,8 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-BC_T7R89.css') }}">
+    <script src="{{ secure_asset('build/assets/app-z-Rg4TxU.js') }}"></script>
     @include('modals.auth')
     @include('modals.register')
     <style>
