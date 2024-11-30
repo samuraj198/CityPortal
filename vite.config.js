48306fcbc225085b585e56042843f3613cde978e
samuraj198 <daniil885@mail.ru>
2024-11-30 21:17:13 +0400

    change vite file

--- vite.config.js
+++ vite.config.js
@@ -6,11 +6,18 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
-        }),
+        }),пше
     ],
     build: {
         manifest: true, // Генерация манифеста для Laravel
         outDir: 'public/build', // Путь до директории вывода
+        rollupOptions: {
+            output: {
+                entryFileNames: 'assets/[name]-[hash].js',
+                chunkFileNames: 'assets/[name]-[hash].js',
+                assetFileNames: 'assets/[name]-[hash][extname]',
+            },
+        },
     },
     server: {
         https: true,
