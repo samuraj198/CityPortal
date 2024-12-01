cc2fcf6b4c244ad49d17dc2ca484dccd0f3fd7f3
samuraj198 <daniil885@mail.ru>
2024-12-01 16:56:06 +0400

    вернул storage in gitignore

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -96,12 +96,12 @@
         <div class="notifs-case absolute top-0 left-0 w-full z-50 flex justify-center p-10">
             @if(session('success'))
                 <div class="success fixed max-w-96 hidden bg-button text-white px-5 py-3 rounded-lg top-5 z-50">
-                    <p class="break-words px-5 py-3">{{ session('success') }}</p>
+                    <p class="break-words">{{ session('success') }}</p>
                 </div>
             @elseif($errors->any())
                 <div class="error fixed hidden max-w-96  bg-black text-white px-5 py-3 rounded-lg top-5 z-50">
                     @foreach($errors->all() as $error)
-                        <p class="break-words px-5 py-3">{{ $error }}</p>
+                        <p class="break-words">{{ $error }}</p>
                     @endforeach
                 </div>
             @endif
